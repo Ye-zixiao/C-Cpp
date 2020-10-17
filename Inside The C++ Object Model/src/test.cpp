fc97f5b6a945bb4fa4d25678a13459516576b73b
Ye-zixiao <1461018824@qq.com>
2020-10-17 21:48:48 +0800

    date-2020-10-17

--- Inside The C++ Object Model/src/test.cpp
+++ Inside The C++ Object Model/src/test.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 
-class Test{
+class Base1{
 public:
-    Test(int val):
-        value(val){}
+    virtual ~Base1()=default;
+    virtual void speakClearly(){}
+    virtual Base1*clone() const{}
 
-    int func(void) {
-        return value;
-    }
-
-    static int sfunc(){
-        return svalue;
-    }
-private:
-    int value;
-    static int svalue;
+protected:
+    float data_base1;
 };
 
-int Test::svalue=10;
+class Base2{
+public:
+    virtual ~Base2()=default;
+    virtual void mumble() {}
+    virtual Base2*clone() const {}
 
-int main(void)
-{  
-    std::cout<<((Test*)0)->sfunc()<<std::endl;
+protected:
+    float data_base2;
+};
+
+class Derived:public Base1,public Base2{
+public:
+    virtual ~Derived() override{}
+    virtual Derived* clone() const override{}
+
+protected:
+    float data_derived;
+};
+
+class Test{
+public:
+    int value1;
+};
+
+class Test_Derived:public Test{
+public:
+    int value2;
+};
+
+int main(int argc,char*argv[])
+{
+    Derived item;
+    Test_Derived test_derived;
+    std::cout<<"We create derived item"<<std::endl;
+    std::cout<<"Test::value1        : "<<&test_derived.value1<<std::endl;
+    std::cout<<"Test_Derived::value2: "<<&test_derived.value2<<std::endl;
 
     return 0;
 }
