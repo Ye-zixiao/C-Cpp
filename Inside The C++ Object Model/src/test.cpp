2e2e5d4433da8e1d4a15722e49b39a2c9e230ebd
Ye-zixiao <1461018824@qq.com>
2020-10-18 22:47:09 +0800

    date-2020-10-18

--- Inside The C++ Object Model/src/test.cpp
+++ Inside The C++ Object Model/src/test.cpp
@@ -1,51 +1,23 @@
 #include <iostream>
-
-class Base1{
-public:
-    virtual ~Base1()=default;
-    virtual void speakClearly(){}
-    virtual Base1*clone() const{}
-
-protected:
-    float data_base1;
-};
-
-class Base2{
-public:
-    virtual ~Base2()=default;
-    virtual void mumble() {}
-    virtual Base2*clone() const {}
-
-protected:
-    float data_base2;
-};
-
-class Derived:public Base1,public Base2{
-public:
-    virtual ~Derived() override{}
-    virtual Derived* clone() const override{}
-
-protected:
-    float data_derived;
-};
+#include <cstdio>
 
 class Test{
 public:
-    int value1;
+    virtual ~Test(){}
+
+    float x(){return _x;}
+    float y(){return _y;}
+    virtual float z(){return _z;}
+
+private:
+    float _x,_y,_z;
 };
 
-class Test_Derived:public Test{
-public:
-    int value2;
-};
-
-int main(int argc,char*argv[])
+int main(void)
 {
-    Derived item;
-    Test_Derived test_derived;
-    std::cout<<"We create derived item"<<std::endl;
-    std::cout<<"Test::value1        : "<<&test_derived.value1<<std::endl;
-    std::cout<<"Test_Derived::value2: "<<&test_derived.value2<<std::endl;
-
+    printf("%p\n",&Test::x);
+    printf("%p\n",&Test::y);
+    // printf("%p\n",&Test::~Test);
+    printf("%p\n",&Test::z);
     return 0;
 }
