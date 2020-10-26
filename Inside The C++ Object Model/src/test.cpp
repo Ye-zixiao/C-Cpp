d7f11f869b51174d196f72066f06bef20fed6a7a
Ye-zixiao <1461018824@qq.com>
2020-10-26 21:06:03 +0800

    date-2020-10-26

--- Inside The C++ Object Model/src/test.cpp
+++ Inside The C++ Object Model/src/test.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
 #include <typeinfo>
 
-class Base{
-public:
-	virtual void func() const = 0 {
-		std::cout<<std::endl;
-	}
+struct __mptr{
+	int delta;
+	int index;
+	union {
+		void* faddr;
+		int v_offset;
+	};
 };
-
-class Derived:public Base{
-public:
-	void func() const override{
-		Base::func();
-		std::cout<<"fuck"<<std::endl;
-	}
-};
-
-
-int main(void)
-{
-	Derived item;
-	item.func();
-	return 0;
-}
